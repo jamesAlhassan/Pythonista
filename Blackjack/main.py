119ea16912f6650bb1330fb717d37a91c74c2f79
James Alhassan <jamesalhassan052@gmail.com>
2022-12-17 20:19:27 +0000

    main.py updated

--- Blackjack/main.py
+++ Blackjack/main.py
@@ -22,19 +22,32 @@ def calculate_score(cards):
     return sum(cards)
 
 is_game_over = False
-user_cards = []
 
+
+user_cards = []
 computer_cards = []
 
 for card in range(2):
     user_cards.append(deal_card())
     computer_cards.append(deal_card())
 
-user_score = calculate_score(user_cards)
-computer_score = calculate_score(computer_cards)
+
+while is_game_over:
+    user_score = calculate_score(user_cards)
+    computer_score = calculate_score(computer_cards)
+
+    print(f"    Your cards: {user_cards}, current score: {user_score}")
+    print(f"    Computer's first card: {computer_cards[0]}")
 
 
-print(user_score, computer_score)
-if user_score == 0 or computer_score == 0 or user_score > 21:
-    is_game_over = True
-
+    if user_score == 0 or computer_score == 0 or user_score > 21:
+        is_game_over = True
+    else:
+        user_should_deal = input("Type 'y' to get another card or 'n' to pass: ")
+        if user_should_deal == "yes":
+            user_cards.append(deal_card())
+        else:
+            is_game_over == True
+while computer_score != 0 and computer_score < 17:
+    computer_cards.append(deal_card())
+    compter_score = calculate_score(compter_cards)
