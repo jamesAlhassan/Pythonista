a0ffaf1962fadf9f3a1ead531e9b19aa13998419
James Alhassan <jamesalhassan052@gmail.com>
2022-12-17 19:49:19 +0000

    calculate_score added to main.py

--- Blackjack/main.py
+++ Blackjack/main.py
@@ -4,10 +4,24 @@
 from art import logo
 import random
 
+
+# FUNCTIONS
 def deal_card():
     cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10,10, 10]
     return (random.choice(cards))
 
+
+def calculate_score(cards):
+    if sum(cards) == 21 and len(cards) == 2:
+        return 0
+
+    if 11 in cards and sum(cards) > 21:
+        cards.remove(11)
+        cards.append(1)
+
+    return sum(cards)
+
+is_game_over = False
 user_cards = []
 
 computer_cards = []
@@ -16,5 +30,11 @@ for card in range(2):
     user_cards.append(deal_card())
     computer_cards.append(deal_card())
 
-print(user_cards)
+user_score = calculate_score(user_cards)
+computer_score = calculate_score(computer_cards)
+
+
+print(user_score, computer_score)
+if user_score == 0 or computer_score == 0 or user_score > 21:
+    is_game_over = True
 
