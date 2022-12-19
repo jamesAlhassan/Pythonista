108f97acbad304ef3c08835a047f3a3fd907bc7a
James Alhassan <jamesalhassan052@gmail.com>
2022-12-19 06:59:11 +0000

    main.p updated

--- HigherLower/main.py
+++ HigherLower/main.py
@@ -1,3 +1,27 @@
 #!/usr/bin/python3
 
 
+from art import logo, vs
+from game_data import data
+from random import randint as rd
+
+A = rd(0, len(data)) - 1
+B = rd(0, len(data)) - 1
+choice_A = data[A]
+details_A = []
+choice_B = data[B]
+details_B = []
+print(logo)
+
+for k, v in choice_A.items():
+    details_A.append(v)
+
+
+print(f"Compare A: {details_A[0]}, {details_A[2]}, from {details_A[3]}")
+
+print(vs)
+
+for k, v in choice_B.items():
+    details_B.append(v)
+
+print(f"Against B: {details_B[0]}, {details_B[2]}, from {details_B[3]}")
