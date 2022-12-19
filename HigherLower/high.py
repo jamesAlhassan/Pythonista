904e486932b1e8f20de28885eaf43458ac6a7519
James Alhassan <jamesalhassan052@gmail.com>
2022-12-19 09:24:05 +0000

    main.p updated

--- HigherLower/high.py
+++ HigherLower/high.py
@@ -13,16 +13,11 @@ def clear():
 
 A = rd(0, len(data)) - 1
 B = rd(0, len(data)) - 1
-score = 0
-final_score = 0
 
-choice_A = data[rd(0, len(data)) - 1]
-choice_B = data[rd(0, len(data)) - 1]
-
-if choice_A == choice_B:
-    choice_B += 1
-
+choice_A = data[A]
 details_A = []
+
+choice_B = data[B]
 details_B = []
 
 print(logo)
@@ -40,25 +35,20 @@ for k, v in choice_B.items():
 
 print(f"Against B: {details_B[0]}, {details_B[2]}, from {details_B[3]}")
 
-followers =  input("Who has more followers? Type 'A' or 'B': ")
+more_followers = input("Who has more followers? Type 'A' or 'B': ")
 
-
-if followers == 'A' and details_A[1] > details_B[1]:
+score = 0
+final_score = 0
+if more_followers == 'A' and details_A[1] > details_B[1]:
     clear()
     score += 1
     final_score += 1
-    print(logo)
     print(f"You're right! Current score: {score}")
-    print(vs)
-    details_B = details_A
 
-elif followers == 'B' and details_A[1] < details_B[1]:
+elif more_followers == 'B' and details_A[1] < details_B[1]:
     clear()
     score += 1
     final_score += 1
-    print(logo)
-    print(f"You're right! Current score: {score}")
-    print(vs)
-    details_B = details_A
+    print(f"You're right! Current score: {score + 1}")
 else:
     print(f"Sorry, that's wrong. Final score: {final_score}")
