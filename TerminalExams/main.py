c5723b6e42195502fe5a64c3ffdc1580160b7838
James Alhassan <jamesalhassan052@gmail.com>
2022-12-23 21:47:06 +0000

    question_mode.py updated

--- TerminalExams/main.py
+++ TerminalExams/main.py
@@ -5,8 +5,8 @@ from quiz_brain import QuizBrain
 question_bank = []
 
 for question in question_data:
-    question_text = question["text"]
-    answer = question["answer"]
+    question_text = question["question"]
+    answer = question["correct_answer"]
     new_question = Question(question_text, answer)
     question_bank.append(new_question)
 
