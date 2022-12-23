2c1fe87bbf9b18e6330f2c629040e1ef10468e32
James Alhassan <jamesalhassan052@gmail.com>
2022-12-23 21:37:01 +0000

    question_mode.py updated

--- TerminalExams/main.py
+++ TerminalExams/main.py
@@ -14,3 +14,6 @@ for question in question_data:
 quiz = QuizBrain(question_bank)
 while quiz.still_has_questions():
     quiz.next_question()
+
+print("You've completed the quiz")
+print(f"You final score is: {quiz.score}/ {quiz.question_number}")
