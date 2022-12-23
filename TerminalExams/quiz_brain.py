a6356faaa7d1a26bbe8ae6a84a8de247c231c2d1
James Alhassan <jamesalhassan052@gmail.com>
2022-12-23 21:27:33 +0000

    question_mode.py updated

--- TerminalExams/quiz_brain.py
+++ TerminalExams/quiz_brain.py
@@ -2,6 +2,7 @@ class QuizBrain:
     def __init__(self, q_list):
         self.question_number = 0
         self.question_list = q_list
+        self.score = 0
 
     def next_question(self):
         current_question = self.question_list[self.question_number]
@@ -14,7 +15,9 @@ class QuizBrain:
 
     def check_answer(self, user_answer, correct_answer):
         if user_answer.lower() == correct_answer.lower():
+            self.score += 1
             print("You got it right")
+            print(f"Your current score is: {self.score}/{self.score} ")
         else:
             print("That' wrong")
         print(f"The correct answer was: {correct_answer}")
