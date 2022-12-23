de472620c4e35003da971398156e314a59da2338
James Alhassan <jamesalhassan052@gmail.com>
2022-12-23 09:30:06 +0000

    question_mode.py updated

--- TerminalExams/quiz_brain.py
+++ TerminalExams/quiz_brain.py
@@ -1,7 +1,11 @@
-# from main import
 class QuizBrain:
-    def __init__(self):
+    def __init__(self, q_list):
         self.question_number = 0
-        self.question_list = input()
+        self.question_list = q_list
+
+    def next_question(self):
+        current_question = self.question_list[self.question_number]
+        f"Q.{self.question_number: {current_question.text}}"
+
 
 
