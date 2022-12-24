77b61af5e29caf06f9b20bb9ba835cbb4f8037d7
James Alhassan <jamesalhassan052@gmail.com>
2022-12-24 07:43:07 +0000

    question_mode.py updated

--- TerminalExams/question_model.py
+++ TerminalExams/question_model.py
@@ -3,8 +3,8 @@ class Question:
         self.question = question
         self.answer = answer
 
-    def __repr__(self):
-        return f"{self.question} (True/False)"
+    # def __repr__(self):
+    #     return f"{self.question} (True/False)"
 
 
 # question_1 = Question("What is the brain of the computer? ", "CPU")
