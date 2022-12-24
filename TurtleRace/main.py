54101358378a37d4d9cde6701b9e00835f014866
James Alhassan <jamesalhassan052@gmail.com>
2022-12-24 22:52:25 +0000

    main.py updated

--- TurtleRace/main.py
+++ TurtleRace/main.py
@@ -2,40 +2,18 @@ from turtle import Turtle, Screen
 from random import choice as ch
 
 colors =["red", "orange", "yellow", "blue", "green", "purple"]
+y_positions = [0, 30, 60, 90, -30, -60]
 screen = Screen()
 screen.setup(width=500, height=400)
 user_bet = screen.textinput("Make your bet", prompt="Who's going to win this turtle race? Enter a color: ")
 
+for timmy in range(len(colors)):
+    tim = Turtle(shape="turtle")
+    tim.penup()
+    tim.color(colors[timmy])
+    tim.goto(x=-235, y=y_positions[timmy])
 
-reddy = Turtle(shape="turtle")
-reddy.penup()
-reddy.color("red")
-reddy.goto(x=-235, y=0)
 
-orangy = Turtle(shape="turtle")
-orangy.penup()
-orangy.color("orange")
-orangy.goto(x=-235, y=30)
-
-yellowy = Turtle(shape="turtle")
-yellowy.penup()
-yellowy.color("yellow")
-yellowy.goto(x=-235, y=60)
-
-bluewy = Turtle(shape="turtle")
-bluewy.penup()
-bluewy.color("blue")
-bluewy.goto(x=-235, y=90)
-
-greeny = Turtle(shape="turtle")
-greeny.penup()
-greeny.color("green")
-greeny.goto(x=-235, y=-30)
-
-purply = Turtle(shape="turtle")
-purply.penup()
-purply.color("purple")
-purply.goto(x=-235, y=-60)
 
 screen.listen()
 
