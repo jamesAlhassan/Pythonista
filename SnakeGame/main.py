bdf895a0b2b3fc102b82a5b392716f09de566434
James Alhassan <jamesalhassan052@gmail.com>
2022-12-25 12:43:23 +0000

    main.py updated
    turtle setup

--- SnakeGame/main.py
+++ SnakeGame/main.py
@@ -6,6 +6,14 @@ screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake Game")
 
+# Turtle Setup
+pos = [0, -20, -40]
+for x in range(len(pos)):
+    turtle = Turtle(shape="square")
+    turtle.color("white")
+    turtle.goto(x=pos[x], y=0)
+
+
 
 
 
