911cd850ec1eaea02775994f865f011d4570ae43
James Alhassan <jamesalhassan052@gmail.com>
2022-12-25 13:15:15 +0000

    main.py updated

--- SnakeGame/main.py
+++ SnakeGame/main.py
@@ -24,8 +24,14 @@ game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-    for sn in snakes:
-        sn.fd(20)
+
+    for sn in range(len(snakes) - 1, 0, -1):
+        new_x = snakes[sn - 1].xcor()
+        new_y = snakes[sn - 1].ycor()
+        snakes[sn].goto(x=new_x, y=new_y)
+    snakes[0].fd(20)
+    # snakes[0].lt(90)
+
 
 
 
