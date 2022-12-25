7a92e30f41e4caad16711da5700f540e2cacb77d
James Alhassan <jamesalhassan052@gmail.com>
2022-12-25 13:03:48 +0000

    main.py updated

--- SnakeGame/main.py
+++ SnakeGame/main.py
@@ -1,3 +1,4 @@
+import time
 from turtle import Turtle, Screen
 
 # screen Setup
@@ -5,13 +6,26 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake Game")
+screen.tracer(0)
 
 # Turtle Setup
+snakes = []
 pos = [0, -20, -40]
 for x in range(len(pos)):
-    turtle = Turtle(shape="square")
-    turtle.color("white")
-    turtle.goto(x=pos[x], y=0)
+    snake = Turtle(shape="square")
+    snake.color("white")
+    snake.penup()
+    snake.goto(x=pos[x], y=0)
+    snakes.append(snake)
+
+
+game_is_on = True
+
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    for sn in snakes:
+        sn.fd(20)
 
 
 
