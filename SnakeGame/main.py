acf5f4631d312829af17512430d3b6270096db15
James Alhassan <jamesalhassan052@gmail.com>
2022-12-25 12:29:33 +0000

    main.py added
    screen setip

---
+++ SnakeGame/main.py
@@ -0,0 +1,14 @@
+from turtle import Turtle, Screen
+
+# screen Setup
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snake Game")
+
+
+
+
+
+
+screen.exitonclick()
