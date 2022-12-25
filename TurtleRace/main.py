02416f0e6b33c536a0fad61f45465631a6d2ac20
James Alhassan <jamesalhassan052@gmail.com>
2022-12-25 05:59:55 +0000

    main.py updated

--- TurtleRace/main.py
+++ TurtleRace/main.py
@@ -1,17 +1,25 @@
 from turtle import Turtle, Screen
 from random import choice as ch
 
+
+is_race_on = False
 colors =["red", "orange", "yellow", "blue", "green", "purple"]
 y_positions = [0, 30, 60, 90, -30, -60]
 screen = Screen()
 screen.setup(width=500, height=400)
 user_bet = screen.textinput("Make your bet", prompt="Who's going to win this turtle race? Enter a color: ")
+all_turtles = []
 
 for timmy in range(len(colors)):
-    tim = Turtle(shape="turtle")
-    tim.penup()
-    tim.color(colors[timmy])
-    tim.goto(x=-235, y=y_positions[timmy])
+    new_turtle = Turtle(shape="turtle")
+    new_turtle.penup()
+    new_turtle.color(colors[timmy])
+    new_turtle.goto(x=-235, y=y_positions[timmy])
+    all_turtles.append(new_turtle)
+
+
+if user_bet:
+    is_race_on = True
 
 
 
