b8a048e03bfa34d9de3707d6263e5c9ab9658a9c
James Alhassan <jamesalhassan052@gmail.com>
2022-12-27 17:59:52 +0000

    scoreboard.py updated

--- SnakeGame/scoreboard.py
+++ SnakeGame/scoreboard.py
@@ -20,4 +20,8 @@ class Score(Food):
         self.clear()
         self.write(f"Score: {self.score}", align=ALIGN, font=FONT)
 
+    def game_over(self):
+        self.goto(0, 0)
+        self.write(arg="GAME OVER", align=ALIGN, font=FONT)
+
 
