4ecb881363dc501d36ac95d47d5cf048d81c6fce
James Alhassan <jamesalhassan052@gmail.com>
2022-12-28 10:35:50 +0000

    main.py

--- PongGame/main.py
+++ PongGame/main.py
@@ -1,10 +1,17 @@
-from turtle import Screen
+from turtle import Screen, Turtle
 # Screen Setup
 screen = Screen()
 screen.setup(width=800, height=600)
 screen.title("Ping Pong")
 screen.bgcolor("black")
 
+
+
+paddle = Turtle(shape="square")
+paddle.shapesize(stretch_wid=5, stretch_len=1)
+paddle.color("white")
+paddle.penup()
+paddle.goto(x=350, y=0)
 
 
 screen.exitonclick()
