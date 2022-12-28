58c9bbe885b9be23a614e0c7fe81aad8f65448ba
James Alhassan <jamesalhassan052@gmail.com>
2022-12-28 13:50:40 +0000

    main.py

--- PongGame/main.py
+++ PongGame/main.py
@@ -1,4 +1,5 @@
 from turtle import Screen, Turtle
+from paddle import Paddle
 # Screen Setup
 screen = Screen()
 screen.setup(width=800, height=600)
@@ -7,7 +8,8 @@ screen.bgcolor("black")
 screen.tracer(0)
 
 
-
+left_paddle = Paddle(350, 0)
+right_paddle = Paddle(-350, 0)
 def move_up():
     new_y = paddle.ycor() + 20
     paddle.goto(paddle.xcor(), new_y)
