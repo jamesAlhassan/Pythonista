26bf42dc719c0485813ae2dc5b31619077f495a7
James Alhassan <jamesalhassan052@gmail.com>
2022-12-28 17:04:45 +0000

    ball.py added

--- PongGame/ball.py
+++ PongGame/ball.py
@@ -1,23 +1,30 @@
-from turtle import  Turtle
+from turtle import Turtle
 
 
 class Ball(Turtle):
+
     def __init__(self):
         super().__init__()
-        # self.goto(x, y)
-        # self.shapesize(stretch_wid=5, stretch_len=1)
+        self.color("white")
         self.shape("circle")
-        self.color("green")
         self.penup()
-        self.x_move = 10
-        self.y_move = 10
+        self.x_move = 3
+        self.y_move = 3
+        self.move_speed = 0.1
 
-    def ball_move(self):
+    def move(self):
         new_x = self.xcor() + self.x_move
         new_y = self.ycor() + self.y_move
         self.goto(new_x, new_y)
 
-    def ball_bounce(self):
+    def bounce_y(self):
         self.y_move *= -1
 
+    def bounce_x(self):
+        self.x_move *= -1
+        self.move_speed *= 0.9
 
+    def reset_position(self):
+        self.goto(0, 0)
+        self.move_speed = 0.1
+        self.bounce_x()
--- PongGame/paddle.py
+++ PongGame/paddle.py
@@ -2,18 +2,19 @@ from turtle import Turtle
 
 
 class Paddle(Turtle):
-    def __init__(self, x: float, y: float):
+
+    def __init__(self, position):
         super().__init__()
-        self.goto(x, y)
-        self.shapesize(stretch_wid=5, stretch_len=1)
         self.shape("square")
         self.color("white")
+        self.shapesize(stretch_wid=5, stretch_len=1)
         self.penup()
+        self.goto(position)
 
-    def move_up(self):
+    def go_up(self):
         new_y = self.ycor() + 20
         self.goto(self.xcor(), new_y)
 
-    def move_down(self):
+    def go_down(self):
         new_y = self.ycor() - 20
         self.goto(self.xcor(), new_y)
