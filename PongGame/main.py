4e80663dbcf50f82ba82cee18d61baabdf080a9d
James Alhassan <jamesalhassan052@gmail.com>
2022-12-28 10:45:25 +0000

    main.py

--- PongGame/main.py
+++ PongGame/main.py
@@ -4,7 +4,7 @@ screen = Screen()
 screen.setup(width=800, height=600)
 screen.title("Ping Pong")
 screen.bgcolor("black")
-
+screen.tracer(0)
 
 
 paddle = Turtle(shape="square")
@@ -21,7 +21,12 @@ def move_down():
     new_y = paddle.ycor() - 20
     paddle.goto(paddle.xcor(), new_y)
 
+
 screen.listen()
 screen.onkey(move_up, "Up")
 screen.onkey(move_down, "Down")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
 screen.exitonclick()
