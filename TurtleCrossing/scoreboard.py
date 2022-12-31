ad87b1a541f9635863b07db572ff9d577f9a1341
James Alhassan <jamesalhassan052@gmail.com>
2022-12-31 19:32:36 +0000

    scoreboard.py updated
    game_over func added

--- TurtleCrossing/scoreboard.py
+++ TurtleCrossing/scoreboard.py
@@ -22,3 +22,7 @@ class Scoreboard(Turtle):
         self.level += 1
         self.update_scoreboard()
 
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("Game Over", font=FONT, align="center")
+
