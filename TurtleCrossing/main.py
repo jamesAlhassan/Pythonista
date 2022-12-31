ed8a4af704c970d013b7fbe1975638ba2d5d7b26
James Alhassan <jamesalhassan052@gmail.com>
2022-12-31 14:54:11 +0000

    player.py updated
    player class

--- TurtleCrossing/main.py
+++ TurtleCrossing/main.py
@@ -4,11 +4,16 @@ from player import Player
 from car_manager import CarManager
 from scoreboard import Scoreboard
 
+
+# Screen Setup
 screen = Screen()
 screen.setup(width=600, height=600)
+# screen.bgcolor("")
 screen.tracer(0)
 
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
+screen.exitonclick()
