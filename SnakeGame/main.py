48380abd486fe1d5807b96754853d13185466b71
James Alhassan <jamesalhassan052@gmail.com>
2023-01-01 23:10:51 +0000

    main.py updated  updated

--- SnakeGame/main.py
+++ SnakeGame/main.py
@@ -34,16 +34,29 @@ while game_is_on:
 
     #Detect collision with wall.
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
-        game_is_on = False
-        scoreboard.game_over()
+        scoreboard.reset_score()
+
+    # Appear from the other side
+    # if snake.head.xcor() == 280:
+    #     y = snake.head.ycor()
+    #     snake.head.goto(x=-280, y=y)
+    # elif snake.head.xcor() == -280:
+    #     y_1 = snake.head.ycor()
+    #     snake.head.goto(x=280, y=y_1)
+    #
+    # if snake.head.ycor() == 280:
+    #     x = snake.head.xcor()
+    #     snake.head.goto(x=x, y=-280)
+    # elif snake.head.ycor() == -280:
+    #     x_1 = snake.head.xcor()
+    #     snake.head.goto(x=x_1, y=280)
 
     #Detect collision with tail.
     for segment in snake.segments:
         if segment == snake.head:
             pass
         elif snake.head.distance(segment) < 10:
-            game_is_on = False
-            scoreboard.game_over()
+            scoreboard.reset_score()
 
 
 
