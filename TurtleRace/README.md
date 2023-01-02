4128dc228dc204c4c5fcd812be6d75f4cc70a901
James Alhassan <jamesalhassan052@gmail.com>
2023-01-02 06:18:55 +0000

    main.py updated  updated

---
+++ TurtleRace/README.md
@@ -0,0 +1,2 @@
+# Turtle Race
+![day19](https://user-images.githubusercontent.com/98851253/154783894-1c7cfc07-0752-402d-800f-afbed8fdea9a.gif)
--- TurtleRace/main.py
+++ TurtleRace/main.py
@@ -24,6 +24,13 @@ if user_bet:
 
 while is_race_on:
     for turtle in all_turtles:
+        if turtle.xcor() > 230:
+            is_race_on = False
+            winning_color = turtle.pencolor()
+            if winning_color == user_bet:
+                print(f"You've won! The {winning_color} turtle is the winner")
+            else:
+                print(f"You've lost! The {winning_color} turtle is the winner")
         rand_distance = random.randint(1, 10)
         turtle.fd(rand_distance)
 
