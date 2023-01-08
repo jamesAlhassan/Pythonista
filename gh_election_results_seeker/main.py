2f758b11c80f134d362aba258dc4254c65c0e797
James Alhassan <jamesalhassan052@gmail.com>
2023-01-08 12:47:22 +0000

    main.py updated

--- gh_election_results_seeker/main.py
+++ gh_election_results_seeker/main.py
@@ -2,6 +2,7 @@ import turtle
 import pandas as pd
 from result import Result
 
+details = [(-401, 420), (-399, 387), (-397, 341), (-399, 298), (-403, 256), (-397, 214)]
 screen = turtle.Screen()
 screen.title("2020 GH Election Results")
 image = "regional.gif"
@@ -14,6 +15,7 @@ all_result = Result()
 
 data = pd.read_csv("presidential_regional_results.csv")
 all_party = data['Party'].to_list()
+all_candidate = data['Candidate'].to_list()
 
 
 
@@ -32,7 +34,11 @@ if search_party in all_party:
     result_data = data[data.Party == search_party]
     result.goto(int(result_data.x), int(result_data.y))
     result.write(f"{result_data.Candidate.item()} \n  {result_data.Party.item()} \n {result_data.Votes.item()}", font=("Courier", 16, "normal"))
-
+    a = 0
+    for detail in details:
+        all_result.goto(detail)
+        all_result.write(f"{all_candidate[a]}")
+        a += 1
 
 
 
