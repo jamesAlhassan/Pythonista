1811095c7f2fb5c4639d26f8e094a9a062770f38
James Alhassan <jamesalhassan052@gmail.com>
2023-01-15 22:25:54 +0000

    main.py updated

--- Pomodoro_Timer/main.py
+++ Pomodoro_Timer/main.py
@@ -15,9 +15,16 @@ LONG_BREAK_MIN = 20
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 
+
+def counter(count):
+    canvas.itemconfig(timer_text, text=count)
+    if count > 0:
+        window.after(1000, counter, count - 1)
 # ---------------------------- UI SETUP ------------------------------- #
 
 # Window/Screen Setup
+
+
 window = Tk()
 window.title('Workout Timer')
 window.config(padx=100, pady=50, bg=YELLOW)
@@ -31,14 +38,16 @@ timer_label.grid(column=1, row=0)
 canvas = Canvas(width=300, height=307, bg=YELLOW, highlightthickness=0)
 canvas_img = PhotoImage(file='work_out.png')
 canvas.create_image(150, 153, image=canvas_img)
-canvas.create_text(190, 50, text='00:00', fill=RED, font=(FONT_NAME, 35, 'bold'))
+timer_text = canvas.create_text(190, 50, text='00:00', fill=RED, font=(FONT_NAME, 35, 'bold'))
 canvas.grid(column=1, row=1)
 
+counter(30)
+
 # Buttons
-start_button =Button(text='Start')
+start_button = Button(text='Start', highlightthickness=0)
 start_button.grid(column=0, row=3)
 
-reset_button =Button(text='Reset')
+reset_button = Button(text='Reset', highlightthickness=0)
 reset_button.grid(column=3, row=3)
 
 
