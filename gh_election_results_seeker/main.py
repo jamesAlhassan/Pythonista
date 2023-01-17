274a43472745ad7857172d6c006f204876db7746
James Alhassan <jamesalhassan052@gmail.com>
2023-01-17 10:08:37 +0000

    updated

--- gh_election_results_seeker/main.py
+++ gh_election_results_seeker/main.py
@@ -16,13 +16,8 @@ result = Result()
 all_result = Result()
 heading = Result()
 
-data = pd.read_csv("presidential_regional_results.csv")
-all_party = data['Party'].to_list()
-all_candidate = data['Candidate'].to_list()
-all_votes = data['Votes'].to_list()
-all_region = data['Region'].to_list()
-print(all_region)
-
+data = pd.read_csv("presidential_regional_results.csv", index_col="Region")
+# print(data)
 
 
 # def get_mouse_coor(x, y):
@@ -31,29 +26,67 @@ print(all_region)
 #
 # turtle.onscreenclick(get_mouse_coor)
 # turtle.mainloop()
+
+# search_party = screen.textinput(title="2020 Election Results", prompt="""Enter Party:
+# 0=NPP,
+# 1=NDC,
+# 2=GUM",
+# 3=CPP,
+# 4=GFP,
+# 5=GCPP,
+# 6=APC,
+# 7=LPG,
+# 8=PNC,
+# 9=PPP
 #
-search_party = screen.textinput(title="2020 Election Results", prompt="Enter Party").upper()
-search_region = screen.textinput(title="2020 Election Results", prompt="Enter Region").capitalize()
+# """)
+# search_region = screen.textinput(title="2020 Election Results", prompt="""Enter Region:
+# 0=AHAFO,
+# 1=ASHANTI,
+# 2=BONO",
+# 3=BONO EAST,
+# 4=CENTRAL,
+# 5=EASTERN,
+# 6=GREATER ACCRA,
+# 7=NORTH EAST,
+# 8=NORTHERN,
+# 9=OTI
+# """)
+search_party = screen.textinput(title="2020 Election Results", prompt="Enter Party: ").upper()
+search_region = screen.textinput(title="2020 Election Results", prompt=" Enter Region: ").capitalize()
+for index, details in data.iterrows():
+    if search_region == index and search_party == details.Party:
+        print(f'{details.Candidate} had {details.Votes} in {search_party} region')
+    # print(details.Party)
+    # print(details.Candidate)
+    # if index == search_region and details.Party == search_party:
+    #     print('yes')
+    #     print(f'{details.Candidate} had {details.Votes} in {search_region} region')
 
-if search_party in all_party and search_region in all_region:
-    heading.color('black')
-    heading.goto(-115, 459)
-    heading.write(f"{search_region} Regional Details")
-
-    result_data = data[data.Party == search_party]
-    result_data1 = data[data.Region == search_region]
-    result.color("red")
-    result.goto(int(result_data.x), int(result_data.y))
-    result.write(f"{result_data.Candidate.item()} \n  {result_data.Party.item()} \n {result_data1.Votes.item()}", font=("Courier", 16, "bold"))
-
-    a = 0
-    for detail in details:
-        all_result.color('green')
-        all_result.goto(detail)
-        all_result.write(f"{all_candidate[a]} \n {all_votes[a]}")
-        a += 1
-
-
+# if search_party:
+#     print(dictt[search_party])
+#     print(dictt['Candidate'][1])
+# if search_party in all_party:
+#     heading.color('black')
+#     heading.goto(-115, 459)
+#     heading.write(f"Regional Details")
+#
+#     result_data = data[data.Party == search_party]
+#     print(result_data)
+#     # result_data1 = data[data.Region == search_region]
+#     result.color("red")
+#     result.goto(int(result_data.x), int(result_data.y))
+#     result.write(f"{result_data.Candidate.item()} \n  {result_data.Party.item()} \n {result_data.Votes.item()}", font=("Courier", 16, "bold"))
+#
+#     a = 0
+#     for detail in details:
+#         all_result.color('green')
+#         all_result.goto(detail)
+#         all_result.write(f"{all_candidate[a]} \n {all_votes[a]}")
+#         a += 1
+#
+#
 
 
 screen.exitonclick()
+
