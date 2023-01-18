4d1d208914312540dde6004bd30f6daed1cb6d40
James Alhassan <jamesalhassan052@gmail.com>
2023-01-18 15:38:34 +0000

    main.py updated: find password func

--- MyPass/main.py
+++ MyPass/main.py
@@ -3,6 +3,9 @@ from tkinter import messagebox
 import random as rd
 import pyperclip
 import json
+
+COLOR = 'white'
+
 # ---------------------------- PASSWORD GENERATOR -------------------------------
 
 
@@ -34,7 +37,6 @@ def generate_password():
 
 
 def save_password_details():
-    num = 0
     website_input = website_entry.get()
     email_input = email_entry.get()
     password_input = password_entry.get()
@@ -67,47 +69,68 @@ def save_password_details():
                 # email_entry.delete(0, END)
                 password_entry.delete(0, END)
         messagebox.showinfo(title='Done', message=f'{website_input} successfully added to the database')
+
+# ---------------------------- SEARCH PASSWORD ------------------------------- #
+def find_password():
+    website_input = website_entry.get()
+    try:
+        with open('data.json') as data_file:
+            data = json.load(data_file)
+            try:
+                website = data[website_input]
+                email = website['email']
+                password = website['password']
+                messagebox.showinfo(title=website_input, message=f'Email: {email}\nPassword: {password}')
+            except KeyError:
+                messagebox.showinfo(title='Error', message=f'No Details found for {website_input}')
+    except FileNotFoundError:
+        messagebox.showinfo(title='Error', message='No Data File Found')
+
+
 # ---------------------------- UI SETUP ------------------------------- #
 
 
 window = Tk()
 window.title('My Password Manager')
-window.config(padx=100, pady=100, bg='white')
+window.config(padx=100, pady=100, bg=COLOR)
 
 
-canvas = Canvas(width=200, height=200, bg='white', highlightthickness=0)
+canvas = Canvas(width=200, height=200, bg=COLOR, highlightthickness=0)
 canvas_img = PhotoImage(file='logo.png')
 canvas.create_image(100, 100, image=canvas_img)
 canvas.grid(row=0, column=1)
 
 # Labels
-website_label = Label(text='Website:', bg='white', pady=6)
+website_label = Label(text='Website:', bg=COLOR, pady=6)
 website_label.grid(row=1, column=0)
 
-email_label = Label(text='Email:', bg='white', pady=6)
+email_label = Label(text='Email:', bg=COLOR, pady=6)
 email_label.grid(row=2, column=0)
 
-password_label = Label(text='Password:', bg='white', pady=6)
+password_label = Label(text='Password:', bg=COLOR, pady=6)
 password_label.grid(row=3, column=0)
 
 
 # Entries
-website_entry = Entry(width=45, bg='white', highlightthickness=0)
-website_entry.grid(row=1, column=1, columnspan=2)
+website_entry = Entry(width=27, bg=COLOR, highlightthickness=0)
+website_entry.grid(row=1, column=1)
 website_entry.focus()
 
-email_entry = Entry(width=45, bg='white', highlightthickness=0)
+email_entry = Entry(width=47, bg=COLOR, highlightthickness=0)
 email_entry.grid(row=2, column=1, columnspan=2)
 email_entry.insert(0, 'example@abc.com')
 
-password_entry = Entry(width=27, bg='white', highlightthickness=0)
+password_entry = Entry(width=27, bg=COLOR, highlightthickness=0)
 password_entry.grid(row=3, column=1)
 
 # Buttons
-generate_button = Button(text='Generate Pass.', width=15, bg='white', command=generate_password)
+generate_button = Button(text='Generate Pass.', width=15, bg=COLOR, command=generate_password)
 generate_button.grid(row=3, column=2)
 
-add_button = Button(text='Add', width=45, bg='white', command=save_password_details)
+add_button = Button(text='Add', width=45, bg=COLOR, command=save_password_details)
 add_button.grid(row=4, column=1, columnspan=2)
+
+search_button = Button(text='Search', width=15, bg=COLOR, command=find_password)
+search_button.grid(row=1, column=2)
 
 window.mainloop()
