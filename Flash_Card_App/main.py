72743361a39c886e00e7c64beca5f8b18b3a6dcc
James Alhassan <jamesalhassan052@gmail.com>
2023-01-19 15:57:52 +0000

    main.py updated: timer func added

--- Flash_Card_App/main.py
+++ Flash_Card_App/main.py
@@ -13,33 +13,36 @@ current_card = {}
 
 
 # Functions
+def timer(count):
+    if count >= 0:
+        timer_label.config(text=f'00:0{count}', bg='black', fg='white')
+        window.after(1000, timer, count - 1)
+
 def next_word():
-    global current_card
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
+    window.after_cancel(timer)
     current_card = choice(data_dict)
-    canvas.itemconfig(language, text='French')
-    canvas.itemconfig(word, text=current_card['French'])
+    canvas.itemconfig(language, text='French', fill='black')
+    canvas.itemconfig(word, text=current_card['French'], fill='black')
     canvas.itemconfig(background, image=front_img)
+    timer(5)
+    flip_timer = window.after(5000, unknown_word)
 
 
-def unkown_word():
-    canvas.itemconfig(language, text='English')
-    canvas.itemconfig(word, text=current_card['English'])
+
+def unknown_word():
+    canvas.itemconfig(language, text='English', fill='white')
+    canvas.itemconfig(word, text=current_card['English'], fill='white')
     canvas.itemconfig(background, image=back_img)
 
-
-def timer(count):
-    timer_label.config(text=f'00:0{count}')
-    if count > 0:
-        window.after(1000, timer, count - 1)
-    else:
-        timer(5)
 
 
 # Window Setup
 window = Tk()
 window.title('Flashy')
 window.config(pady=50, padx=50, bg=BACKGROUND_COLOR)
-window.after(5000, unkown_word)
+flip_timer = window.after(5000, unknown_word)
 # window.after(1000, timer)
 
 # Canvas Setup
@@ -48,25 +51,23 @@ front_img = PhotoImage(file='images/card_front.png')
 back_img = PhotoImage(file='images/card_back.png')
 background = canvas.create_image(400, 263, image=front_img)
 
-language = canvas.create_text(400, 150, text='Language', font=('Arial', 40, 'italic'))
-word = canvas.create_text(400, 263, text='Word', font=('Arial', 60, 'bold'))
+language = canvas.create_text(400, 150, text='', font=('Arial', 40, 'italic'))
+word = canvas.create_text(400, 263, text='', font=('Arial', 60, 'bold'))
 canvas.grid(row=0, column=0, columnspan=3)
 
 # Buttons Setup
 cross_img = PhotoImage(file='./images/wrong.png')
-cross_button = Button(image=cross_img, highlightthickness=0, command=unkown_word)
+cross_button = Button(image=cross_img, highlightthickness=0, command=unknown_word)
 cross_button.grid(row=1, column=0)
 
 check_img = PhotoImage(file='./images/right.png')
 check_button = Button(image=check_img, highlightthickness=0, command=next_word)
 check_button.grid(row=1, column=2)
-next_word()
 
 # timer
-timer_label = Label(text='', bg='black', fg='white')
+timer_label = Label(text='', font=('Helvetica', 40, 'bold'))
 timer_label.grid(row=1, column=1)
 
 next_word()
 timer(5)
-
 window.mainloop()
